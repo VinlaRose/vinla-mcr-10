bb8aef98bca6e135b79506409b85a9ff7a05af63
VinlaRose <109898811+VinlaRose@users.noreply.github.com>
2023-08-12 17:39:16 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-import logo from './logo.svg';
+
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import { Home } from './Pages/Home/Home';
 
 function App() {
   return (
     <div className="App">
       <h1>MCR-11</h1>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+      </Routes>
     </div>
   );
 }
